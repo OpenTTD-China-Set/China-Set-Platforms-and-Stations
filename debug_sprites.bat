786f1c601782b2ce36a8ff3129d10dda57aab400
Jeremy Gao <145064583+WenSimEHRP@users.noreply.github.com>
2024-03-08 20:04:50 -0800

    Change: use gcc rather than nmlpatcher

--- debug_sprites.bat
+++ debug_sprites.bat
@@ -2,7 +2,8 @@
 :start
 py generate_code_and_sprite.py
 py get_date.py
-py nml_patcher.py -f "cnsps.pnml" -o "cnsps.nml" -b 1 -v 1
+::py nml_patcher.py -f "cnsps.pnml" -o "cnsps.nml" -b 1 -v 1
+gcc -E -x c -o cnsps.nml cnsps.pnml
 nmlc cnsps.nml -o cnsplatform.grf
 
 :: move to newgrf directory
